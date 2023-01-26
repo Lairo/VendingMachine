9b385c17e411e4c94e6a7d66bb9e655497924807
chajus1 <chajus1@googlemail.com>
2023-01-26 11:56:35 +0000

    experimenting with virtual/override

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -9,7 +9,9 @@ namespace VendingMachine
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            VendingMachine vendingMachine = new AnimalFeedVendingMachine();
+            Console.WriteLine(vendingMachine.Dispense(2.00M));            
         }
     }
 }
